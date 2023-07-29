64aae0abef3f58d39be1a646a61207bb298bfd48
Sagar Singh Negi <sagarssn23@gmail.com>
2023-07-29 19:55:22 +0530

    Changed Backend to typeScript

--- week-7/assignment-1/routes/todo.ts
+++ week-7/assignment-1/routes/todo.ts
@@ -1,51 +1,61 @@
-const express = require('express');
-const { authenticateJwt, SECRET } = require("../middleware/index");
-const { Todo } = require("../db");
+import express from "express";
+import { authenticateJwt } from "../middleware/";
+import { Todo } from "../db";
 const router = express.Router();
+
+
+interface Todos {
+  title: string;
+  description: string;
+  done: boolean;
+  userId: string;
+}
+
 
 router.post('/todos', authenticateJwt, (req, res) => {
   const { title, description } = req.body;
   const done = false;
-  const userId = req.userId;
+  const userId = req.headers["userId"];
 
   const newTodo = new Todo({ title, description, done, userId });
 
   newTodo.save()
-    .then((savedTodo) => {
+    .then((savedTodo: Todos) => {
       res.status(201).json(savedTodo);
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       res.status(500).json({ error: 'Failed to create a new todo' });
     });
 });
 
 
 router.get('/todos', authenticateJwt, (req, res) => {
-  const userId = req.userId;
+  const userId = req.headers["userId"];
 
   Todo.find({ userId })
-    .then((todos) => {
+    .then((todos: Todos) => {
       res.json(todos);
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       res.status(500).json({ error: 'Failed to retrieve todos' });
     });
 });
 
 router.patch('/todos/:todoId/done', authenticateJwt, (req, res) => {
   const { todoId } = req.params;
-  const userId = req.userId;
+  const userId = req.headers["userId"];
+
 
   Todo.findOneAndUpdate({ _id: todoId, userId }, { done: true }, { new: true })
-    .then((updatedTodo) => {
+    .then((updatedTodo: Todos) => {
       if (!updatedTodo) {
         return res.status(404).json({ error: 'Todo not found' });
       }
       res.json(updatedTodo);
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       res.status(500).json({ error: 'Failed to update todo' });
     });
 });
 
-module.exports = router;
+export default router;
